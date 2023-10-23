a1bbbfb064ef5767fbe64693fb5d7be41a8ffc7e
sap99parmar <parmarsaurav.sp@outlook.com>
2023-10-23 15:02:06 +0530

    qos: Fix merging
    Signed-off-by: sap99parmar <parmarsaurav.sp@outlook.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -199,7 +199,6 @@ static int pm_qos_dbg_show_requests(struct seq_file *s, void *unused)
 	struct pm_qos_constraints *c;
 	struct pm_qos_request *req;
 	char *type;
-	unsigned long flags;
 	int tot_reqs = 0;
 	int active_reqs = 0;
 
@@ -214,11 +213,7 @@ static int pm_qos_dbg_show_requests(struct seq_file *s, void *unused)
 	}
 
 	/* Lock to ensure we have a snapshot */
-<<<<<<< HEAD
-	spin_lock_irqsave(&pm_qos_lock, flags);
-=======
 	raw_spin_lock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 	if (plist_head_empty(&c->list)) {
 		seq_puts(s, "Empty!\n");
 		goto out;
@@ -254,11 +249,7 @@ static int pm_qos_dbg_show_requests(struct seq_file *s, void *unused)
 		   type, pm_qos_get_value(c), active_reqs, tot_reqs);
 
 out:
-<<<<<<< HEAD
-	spin_unlock_irqrestore(&pm_qos_lock, flags);
-=======
 	raw_spin_unlock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 	return 0;
 }
 
@@ -333,16 +324,11 @@ static inline int pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
 int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
 			 enum pm_qos_req_action action, int value)
 {
-	unsigned long flags;
 	int prev_value, curr_value, new_value;
 	struct cpumask cpus;
 	int ret;
 
-<<<<<<< HEAD
-	spin_lock_irqsave(&pm_qos_lock, flags);
-=======
 	raw_spin_lock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 	prev_value = pm_qos_get_value(c);
 	if (value == PM_QOS_DEFAULT_VALUE)
 		new_value = c->default_value;
@@ -374,11 +360,7 @@ int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
 	pm_qos_set_value(c, curr_value);
 	ret = pm_qos_set_value_for_cpus(c, &cpus);
 
-<<<<<<< HEAD
-	spin_unlock_irqrestore(&pm_qos_lock, flags);
-=======
 	raw_spin_unlock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 
 	trace_pm_qos_update_target(action, prev_value, curr_value);
 
@@ -431,14 +413,9 @@ bool pm_qos_update_flags(struct pm_qos_flags *pqf,
 			 struct pm_qos_flags_request *req,
 			 enum pm_qos_req_action action, s32 val)
 {
-	unsigned long irqflags;
 	s32 prev_value, curr_value;
 
-<<<<<<< HEAD
-	spin_lock_irqsave(&pm_qos_lock, irqflags);
-=======
 	raw_spin_lock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 
 	prev_value = list_empty(&pqf->list) ? 0 : pqf->effective_flags;
 
@@ -461,11 +438,7 @@ bool pm_qos_update_flags(struct pm_qos_flags *pqf,
 
 	curr_value = list_empty(&pqf->list) ? 0 : pqf->effective_flags;
 
-<<<<<<< HEAD
-	spin_unlock_irqrestore(&pm_qos_lock, irqflags);
-=======
 	raw_spin_unlock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 
 	trace_pm_qos_update_flags(action, prev_value, curr_value);
 	return prev_value != curr_value;
@@ -500,16 +473,11 @@ EXPORT_SYMBOL_GPL(pm_qos_request_active);
 
 int pm_qos_request_for_cpumask(int pm_qos_class, struct cpumask *mask)
 {
-	unsigned long irqflags;
 	int cpu;
 	struct pm_qos_constraints *c = NULL;
 	int val;
 
-<<<<<<< HEAD
-	spin_lock_irqsave(&pm_qos_lock, irqflags);
-=======
 	raw_spin_lock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 	c = pm_qos_array[pm_qos_class]->constraints;
 	val = c->default_value;
 
@@ -528,11 +496,7 @@ int pm_qos_request_for_cpumask(int pm_qos_class, struct cpumask *mask)
 			break;
 		}
 	}
-<<<<<<< HEAD
-	spin_unlock_irqrestore(&pm_qos_lock, irqflags);
-=======
 	raw_spin_unlock(&pm_qos_lock);
->>>>>>> 501526d02214 (qos: Convert pm_qos_lock to raw spin lock)
 
 	return val;
 }
